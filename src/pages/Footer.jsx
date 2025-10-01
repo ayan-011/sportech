d44f9653202602ecfbf2bb8aad08bc7994f02719
ayan-011 <ayansaifi2nd@gmail.com>
2025-10-01 20:29:33 +0530

    fic bug

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,6 +1,6 @@
 
 import { FloatingDock } from '@/components/ui/floating-dock'; 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const items = [
   {
@@ -63,8 +63,7 @@ export default function Page() {
           />
         </div>
         <div className="detail w-full flex items-center justify-center py-3 text-zinc-400 text-[12px]">Ph: +1 234 567 890</div>
-          
-          <Link to="/admin">Heloo</Link>
+           
 
 
         {/* for mobile  */}
@@ -76,9 +75,8 @@ export default function Page() {
             <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook-icon lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg></a>
             <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" /><rect x="2" y="4" width="20" height="16" rx="2" /></svg></a>
           </div>
+ 
 
-           <Link to="/admin">Heloo</Link>
-           
         </div>
 
       </div>
